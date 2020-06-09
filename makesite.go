bd20ee4596c399a132d9393bd89fac0bec74d133
acidicnic <nic4096@gmail.com>
2020-06-08 18:56:43 -0700

    execution time and file sizes displayed

--- makesite.go
+++ makesite.go
@@ -9,7 +9,63 @@ import (
 	"fmt"
 	"strings"
 	"path/filepath"
+	"github.com/fatih/color"
+	"time"
 )
+
+func main() {
+	start := time.Now()
+
+	fileName := flag.String("file", "", "The name of your .txt file containing data to be inserted into the template.")
+	dirName := flag.String("dir", ".", "The name of your .txt file containing data to be inserted into the template.")
+	flag.Parse()
+
+	var files []string
+	ext := ".txt"
+	if *fileName != "" {
+		files = append(files, *fileName)
+	} else {
+		files = findFilesRec(*dirName, ext)
+	}
+
+	var fileSizes float64
+	for _, fileName := range files {
+		data := readFile(fileName+ext)
+		fileSizes += createAndSaveFile(fileName, data)
+	}
+	s := ""
+	if len(files) > 1 {
+		s = "s"
+	}
+	boldGreen := color.New(color.FgGreen, color.Bold).SprintFunc()
+	bold := color.New(color.Bold).SprintFunc()
+	exeTime := time.Since(start).Seconds()
+	fmt.Printf("%s Generated %s page%s (%.1fkB total) in %.2f seconds.\n", boldGreen("Success!"), bold(len(files)), s, fileSizes, exeTime)
+}
+
+func findFilesRec(dir string, ext string) []string {
+	var files []string
+
+	err := filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+		    if err != nil {
+		        return err
+		    }
+			if filepath.Ext(path) == ext {
+			    files = append(files, path[:len(path)-4])
+			}
+		    return nil
+		})
+	if err != nil {
+	    exit(fmt.Sprintf("Failed to %s\n", err))
+	}
+	return files
+}
+
+type siteData struct {
+    Content string
+	Title string
+}
 
 func readFile(filename string) siteData {
 	fileContents, err := ioutil.ReadFile(filename)
@@ -30,7 +86,7 @@ func readFile(filename string) siteData {
 	return siteData{Content: strings.TrimSpace(string(content)), Title: strings.TrimSpace(string(title))}
 }
 
-func createAndSaveFile(filename string, data siteData) {
+func createAndSaveFile(filename string, data siteData) float64 {
 	paths := []string{"template.tmpl"}
 	buffer := new(bytes.Buffer)
 	t := template.Must(template.New("template.tmpl").ParseFiles(paths...))
@@ -43,46 +99,7 @@ func createAndSaveFile(filename string, data siteData) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func findFilesRec(dir string, ext string) []string {
-	var files []string
-
-	err := filepath.Walk(dir,
-		func(path string, info os.FileInfo, err error) error {
-		    if err != nil {
-		        return err
-		    }
-			if filepath.Ext(path) == ext {
-			    files = append(files, path[:len(path)-4])
-			}
-		    return nil
-		})
-	if err != nil {
-	    exit(fmt.Sprintf("Failed to %s\n", err))
-	}
-	fmt.Println(files)
-	return files
-}
-
-type siteData struct {
-    Content string
-	Title string
-}
-
-func main() {
-	// fileName := flag.String("file", "content", "The name of your .txt file containing data to be inserted into the template.")
-	dirName := flag.String("dir", ".", "The name of your .txt file containing data to be inserted into the template.")
-	flag.Parse()
-
-	ext := ".txt"
-
-	files := findFilesRec(*dirName, ext)
-
-	for _, fileName := range files {
-		data := readFile(fileName+ext)
-		createAndSaveFile(fileName, data)
-	}
+	return float64(len(bytesToWrite)) / float64(1000)
 }
 
 func exit(msg string) {
